e184265e3220f0fdd45a338caca5399c5f278356
Prabhat <prabhatc7879@gmail.com>
2022-09-07 13:44:46 +0530

    index updated

--- answer_fission-backend/src/index.js
+++ answer_fission-backend/src/index.js
@@ -2,8 +2,10 @@ const mongoose=require('mongoose');
 const app=require('./app');
 
 const dbConnectionString="mongodb://localhost:27017/answerFissionDB";
-mongoose.connect(dbConnectionString,{ useNewUrlParser: true, useUnifiedTopology: true },()=>{
+mongoose.connect(dbConnectionString,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
 	console.log("Connected to Database!");
+}).catch((e)=>{
+	console.log("Not Connected");
 });
 
 const portNumber=8888;
